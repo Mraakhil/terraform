7f4b952d0c0def61a5f06f725c21ad91a8baeae7
Mohammad Aakhil <149184615+Mraakhil@users.noreply.github.com>
2025-03-15 02:37:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform{
+bucket "s3"
+  bucket = "habhaiyes3buckethaiuserkr"
+  ker    = "key/terraform.tfstate"
+  region = "ap-south-1"}
+
+
+
+
 resource "aws_instance" "AWSServer" {
   ami             = "ami-02ddb77f8f93ca4ca"
   instance_type   = "t2.micro"
